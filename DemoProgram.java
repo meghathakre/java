fcc34e322782ce5f83657cc9c8130ce6d2b4416c
meghathakre <megha1234567891@gmail>
2024-09-13 22:59:38 +0530

    first commit

---
+++ DemoProgram.java
@@ -0,0 +1,9 @@
+//this is my first program using java
+
+class DemoProgram {
+public static void main(String args[])
+{
+System.out.println("Hello world");
+System.out.println("This is java programming");
+}
+}
---
+++ NameProgram.java
@@ -0,0 +1,11 @@
+
+class NameProgram {
+public static void main(String args[])
+{
+System.out.println("Name:megha");
+System.out.println("0108765543211");
+System.out.println("7879334567");
+System.out.println("megha1234561@gmail.com");
+System.out.println("Bhopal");
+}
+}
